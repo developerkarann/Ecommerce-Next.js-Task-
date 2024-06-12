2143332bbcdb82404b322b800f5065e937fbb4b5
Karan Pal <karanpal03040@gmail.com>
2024-06-12 22:03:23 +0530

    Update Readme and create loader

--- README.md
+++ README.md
@@ -1,46 +1,48 @@
 # Interview Task Assigmnet
 
-Dynamic Web Page Assignment Submission
-Overview
+Dynamic Web Page Assignment Submission Overview
 
-This submission showcases a dynamic web page created using the Next.js framework as per the assignment requirements. The web page consists of a header component, a menu component, and a main component that lists product cards. The product data is fetched from a given API using Axios. The project is hosted on Netlify, and the codebase is available on GitHub.
 
-Features
+This submission showcases a dynamic web page created using the Next.js framework as per the assignment requirements. 
+The web page consists of a header component, a menu component, and a main component that lists product cards. 
+The product data is fetched from a given API using Axios. 
+The project is hosted on Netlify, and the codebase is available on GitHub.
+
+
+Features: 
+
 Header Component: header that contains logo on left side and icons for search, bookmark etc.
+
 Menu Component: An menu section for different different categories of products for easy navigation.
+
 Main Component: Displays product cards with data fetched from an API.
+
 API Integration: Utilizes Axios for fetching product data from the provided API.
+
 Next.js: A powerful React framework for building fast and scalable web applications.
+
 React: A JavaScript library for building user interfaces.
+
 Axios: A promise-based HTTP client for making API requests.
+
 Tailwind Css: For scoped and modular CSS styling.
+
 Netlify: A platform for deploying and hosting the web application.
 
+
 API Integration
-The product data is fetched from an external API using Axios. And storing data in the state variable using useState hook and using useEffect hook for fetching data when webpage is getting render. Here is an example of how the API request is made:
-
+The product data is fetched from an external API using Axios. And storing data into the state variable using useState hook and using useEffect hook for fetching data when webpage is getting render.
 -------------------------
-import axios from 'axios';
-
-  //Feting data from the given API
-  const fetchData = async () => {
-    try {
-      const resposne = await axios.get('https://fakestoreapi.com/products');
-      const { data } = resposne;
-      setProductData(data.slice(0, 8));
-      // API is giving the of 25 producst so using array.slice() method to show only 8 product as it was required 
-    } catch (error) {
-      console.log('Getting while fetching data', error.message)
-    }
-  }
----------------------
 
 Deployment
-The project is deployed on Netlify. You can view the live application using the following link: Live Demo
+The project is deployed on Netlify. You can view the live application using the following link:  https://assignmentforbytequestsoftwares.netlify.app/
 
 
-Thank you so much for this wonderfull opportunity. I look forward to hearings from you soon.  
 
-Karan Pal - karanpal03040@gmail.com
-Phone no. - 91+ 8869012507
-portfolio - https://karandeveloper.vercel.app
+
+Thank you so for this wonderfull opportunity to work on this assignment. I look forward to hearings from you soon.  
+
+Karan Pal
+karanpal03040@gmail.com
+91+8869012507
+https://karandeveloper.vercel.app
--- app/components/MainComponent.jsx
+++ app/components/MainComponent.jsx
@@ -31,34 +31,51 @@ const MainComponent = () => {
           <div className="totalProduct">{productData.length} Products</div>
         </div>
         <div className="products ">
-          <div className="cardContainer grid pl-10 grid-cols-4 my-5  " >
-            {
-              productData?.map((product) => {
-                return (
-                  <>
-                    <div className="card self-center max-w-72  rounded-lg overflow-hidden shadow-lg mb-10  cursor-pointer " style={{ backgroundColor: '#111', maxHeight: '600px' }}>
-                      <img src={product.image} alt="Product Image" className="w-full max-h-96 min-h-96 object-cover" />
-                      <div className="px-3 py-4">
-                        <h2 className=" text-lg mb-2">{product.title.substring(0, 25)}...</h2>
-                        {/* Using array.substring() method because the every product has different length of title 
-                        so it was effecting the widh of card and giving bad user experince  */}
-                        <div className="detaols flex justify-between items-center" >
-                          <div className="flex my-2  items-end">
-                            <p className=" text-base text-xl mr-2 " >₹{product.price}</p>
-                            <p className="text-gray-500  text-xs line-through mr-1 mb-1">₹ {product.price * 2}</p>
-                            <p className=" text-green-700 text-xs mr-1 mb-1">(50% Off)</p>
-                          </div>
-                          <div className="div">
-                            <img src="./assests/store.png" className=' w-7 h-8 ' alt="" />
+          {
+            productData.length >= 1 ? <>
+              <div className="cardContainer grid pl-10 grid-cols-4 my-5" >
+                {
+                  productData?.map((product) => {
+                    return (
+                      <>
+                        <div className="card self-center max-w-72  rounded-lg overflow-hidden shadow-lg mb-10  cursor-pointer " style={{ backgroundColor: '#111', maxHeight: '600px' }}>
+                          <img src={product.image} alt={product.title} className="w-full max-h-96 min-h-96 object-cover" />
+                          <div className="px-3 py-4">
+                            <h2 className=" text-lg mb-2">{product.title.substring(0, 25)}...</h2>
+                            {/* Using array.substring() method because the every product has different length of title 
+                                  so it was effecting the widh of card and giving bad user experince  */}
+                            <div className="detaols flex justify-between items-center" >
+                              <div className="flex my-2  items-end">
+                                <p className=" text-base text-xl mr-2 " >₹{product.price}</p>
+                                <p className="text-gray-500  text-xs line-through mr-1 mb-1">₹ {product.price * 2}</p>
+                                <p className=" text-green-700 text-xs mr-1 mb-1">(50% Off)</p>
+                              </div>
+                              <div className="div">
+                                <img src="./assests/store.png" className=' w-7 h-8 ' alt="" />
+                              </div>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </>
-                )
-              })
-            }
-          </div>
+                      </>
+                    )
+                  })
+                }
+              </div>
+            </> :
+              <>
+                <div className="flex items-center justify-center mt-10">
+
+                  <div class="text-center">
+                    <div
+                      class="w-16 h-16 border-4 border-dashed rounded-full animate-spin  border-white mx-auto"
+                    ></div>
+                    <h2 class="text-zinc-900 dark:text-white mt-4">Loading Products...</h2>
+
+                  </div>
+                </div>
+              </>
+          }
+
         </div>
       </div>
     </>
